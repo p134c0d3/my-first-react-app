e6a7132bdc308cdcb5aa9a2ef97baf233bb472c1
John Pierce <70922864+p134c0d3@users.noreply.github.com>
2025-09-04 10:56:01 -0500

    Add Tailwind CSS integration and implement movie search functionality
    - Integrated Tailwind CSS into the project by adding necessary dependencies and configuring Vite.
    - Developed the App component to fetch and display movies from the TMDB API, including error handling and loading states.
    - Created reusable components: MovieCard, Search, and Spinner for better code organization.
    - Added new assets including images and SVG icons for enhanced UI.
    - Updated styles in index.css to support the new design and layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,86 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+import MovieCard from "./components/MovieCard";
+import Search from "./components/search";
+import Spinner from "./components/spinner";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+	method: "GET",
+	headers: {
+		accept: "application/json",
+		Authorization: `Bearer ${API_KEY}`,
+	},
+};
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const [searchTerm, setsearchTerm] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	const [movies, setMovies] = useState([]);
+	const [loading, setLoading] = useState(false);
 
-export default App
+	const fetchMovies = async () => {
+		setLoading(true);
+		setErrorMessage("");
+		try {
+			const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+			const response = await fetch(endpoint, API_OPTIONS);
+			if (!response.ok) {
+				throw new Error("Failed to fetch movies");
+			}
+			const data = await response.json();
 
+			if (data.response === "False") {
+				setErrorMessage(data.error || "Failed to fetch movies");
+				setMovies([]);
+				return;
+			}
+			setMovies(data.results || []);
+		} catch (error) {
+			console.error("Error fetching movies: ", error);
+			setErrorMessage("Error fetching movies. Please try again.");
+		} finally {
+			setLoading(false);
+		}
+	};
+	useEffect(() => {
+		fetchMovies();
+	}, []);
+
+	return (
+		<main>
+			<div className="pattern" />
+			<div className="wrapper">
+				<header>
+					<img src="./hero.png" alt="Here Banner" />
+					<h1>
+						Find <span className="text-gradient">Movies</span>{" "}
+						You'll Enjoy Without the Hassle
+					</h1>
+					<Search
+						searchTerm={searchTerm}
+						setsearchTerm={setsearchTerm}
+					/>
+				</header>
+
+				<section className="all-movies">
+					<h2 className="mt-[40px]">All Movies</h2>
+					{loading ? (
+						<Spinner />
+					) : errorMessage ? (
+						<p className="text-red-500">{errorMessage}</p>
+					) : (
+						<ul>
+							{movies.map((movie) => (
+								<MovieCard key={movie.id} movie={movie} />
+							))}
+						</ul>
+					)}
+				</section>
+			</div>
+		</main>
+	);
+};
+
+export default App;
